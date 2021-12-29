ad27f129646342f8c0fbe17d6a126ad2f663c79b
Oaya <aya_japan201@yahoo.co.jp>
2021-12-29 04:48:37 +0000

    Declaring and Calling Functions

--- hello.js
+++ hello.js
@@ -4,3 +4,15 @@ const sayHello = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+};
+sayHelloToConsole("John");
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+};
+
+const greeting = returnSayHello("John");
+console.log(greeting);
